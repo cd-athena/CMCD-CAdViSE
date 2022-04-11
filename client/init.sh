58eef46669e0e8df61231e3bd63837aee7850070
Babak <babak.taraghi@gmail.com>
2022-04-11 10:10:05 +0200

    changes client image tag name to cmcd

--- client/init.sh
+++ client/init.sh
@@ -8,7 +8,7 @@ sudo git clone https://github.com/cd-athena/wondershaper.git /home/ec2-user/wond
 config=$(cat /home/ec2-user/config.json)
 player=$(echo "$config" | jq -r '.player')
 
-sudo docker pull babakt/ppt-client:latest &>/dev/null
+sudo docker pull babakt/ppt-client:cmcd &>/dev/null
 sudo docker run --rm -d --name "ppt-client-$player" -p 5900:5900 -v /dev/shm:/dev/shm babakt/ppt-client
 
 sudo docker cp /home/ec2-user/config.json "ppt-client-$player:/home/seluser/ppt/config.json"
