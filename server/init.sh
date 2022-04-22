2b230ab76fe9409d47cb3893d620918d2c635fb0
Babak <babak.taraghi@gmail.com>
2022-04-22 12:18:40 +0200

    use cmcd-enabled image as CAdViSE server

--- server/init.sh
+++ server/init.sh
@@ -5,10 +5,8 @@ sudo yum -y install docker jq git &>/dev/null
 sudo service docker start
 sudo git clone https://github.com/cd-athena/wondershaper.git /home/ec2-user/wondershaper
 
-config=$(cat /home/ec2-user/config.json)
-title=$(echo "$config" | jq -r '.title')
 
-sudo docker pull "babakt/ppt-server-$title:latest" &>/dev/null
-sudo docker run --rm -d --name ppt-server -p 80:80 -v /dev/shm:/dev/shm "babakt/ppt-server-$title"
+sudo docker pull "babakt/ppt-server:cmcd" &>/dev/null
+sudo docker run --rm -d --name ppt-server -p 80:80 -v /dev/shm:/dev/shm "babakt/ppt-server:cmcd"
 
 exit 0
