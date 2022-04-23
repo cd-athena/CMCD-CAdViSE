6c86a67aa4c034ef16693ab2f36a616ac27b54fb
Babak <babak.taraghi@gmail.com>
2022-04-23 12:25:22 +0200

    move docker image to another mount point and increase EC2 size

--- server/init.sh
+++ server/init.sh
@@ -2,9 +2,12 @@
 
 sudo yum -y install docker jq git &>/dev/null
 
+sudo mv /var/lib/docker /dev/
+sudo mkdir /var/lib/docker
+sudo mount --rbind /dev/docker /var/lib/docker
+
 sudo service docker start
 sudo git clone https://github.com/cd-athena/wondershaper.git /home/ec2-user/wondershaper
-
 
 sudo docker pull "babakt/ppt-server:cmcd" &>/dev/null
 sudo docker run --rm -d --name ppt-server -p 80:80 -v /dev/shm:/dev/shm "babakt/ppt-server:cmcd"
