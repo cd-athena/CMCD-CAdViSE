2528410c414945e73f26a4f4afe73f88f91740c6
minhkstn <minhnguyenkstn@gmail.com>
2022-06-21 22:52:04 +0700

    implement the whole idea

--- server/index.js
+++ server/index.js
@@ -26,8 +26,9 @@ app.get('/:title/:fileName', (request, response) => {
   }
 
   if (CMCDParams.dt && CMCDParams.sw && CMCDParams.tb) {
-    console.log('Serving', title, 'manifest_' + getMaxBitrateInMPD(CMCDParams.dt, CMCDParams.sw, CMCDParams.tb) + '.mpd')
-    fs.createReadStream('manifests/stc/manifest_' + getMaxBitrateInMPD(CMCDParams.dt, CMCDParams.sw, CMCDParams.tb) + '.mpd').pipe(response)
+    const finalMaxBitrate = getMaxBitrateInMPDMultipleClient(CMCDParams.dt, CMCDParams.sw, CMCDParams.tb, clientID)
+    console.log('Serving', title, 'manifest_' + finalMaxBitrate + '.mpd')
+    fs.createReadStream('manifests/stc/manifest_' + finalMaxBitrate + '.mpd').pipe(response)
   } else {
     console.log('Serving', title, fileName)
     // fs.createReadStream('dataset/' + title + '/' + fileName).pipe(response)
@@ -75,7 +76,9 @@ const resolutionWidth = [
   '3840'
 ]
 
-const getMaxBitrateInMPD = (deviceType, screenWidth, topBitrate) => {
+const clientData = {}
+
+const getMaxBitrateInMPDSignleClient = (deviceType, screenWidth, topBitrate) => {
   let maxWidthForDevice = 0
   switch (deviceType) {
     case 't':	// tv
@@ -107,4 +110,90 @@ const getMaxBitrateInMPD = (deviceType, screenWidth, topBitrate) => {
   }
 
   return maxBitrateInMPD
+}
+
+// sample client data =
+// {
+//     'm': {                 // mobile
+//         'p0': [720, 4000], // <client_ID>: [<screenWidth>, <tb>]
+//         'p1': [720, 5000]
+//     },
+//     'd': {                 // desktop
+//         'p0': [1080, 7000],
+//         'p1': [1080, 9000]
+//     },
+//     't': {                 // tv
+//         'p0': [2160, 20000],
+//         'p1': [2160, 25000]
+//     }
+// }
+const getMaxBitrateInMPDMultipleClient = (deviceType, screenWidth, topBitrate, clientID) => {
+  console.log('---- deviceType: ' + deviceType)
+  console.log('---- clientD: ' + clientID);
+  console.log('---- clientData[' + deviceType + ']: ' + JSON.stringify(clientData[deviceType]))
+  if (typeof clientData[deviceType] === 'undefined') {
+    clientData[deviceType] = {
+      [clientID] : [screenWidth, topBitrate]
+    }
+    console.log('---- First clientData: ' + JSON.stringify(clientData))
+  }
+  else {
+    clientData[deviceType][clientID] = [screenWidth, topBitrate]
+  }
+
+  console.log('---- CURRENT clientData: ' + JSON.stringify(clientData))
+
+  let data = new Array()
+
+  for (let key in clientData[deviceType]) {
+    let val = clientData[deviceType][key]
+    data.push(val)
+  }
+
+  if (data.length == 1) {
+    console.log('========== data length = 1 ----- END')
+    return getMaxBitrateInMPDSignleClient(deviceType, screenWidth, topBitrate)
+  }
+
+  const k = getK(data)
+  const clusters = kmeans(data, k)
+  let location = -1
+
+  for (let i = 0; i < clusters.centroids.length; i++) {
+    console.log(' clusters idx ' + i + ': ' + JSON.stringify(clusters.clusters[i]))
+
+    const pointsArray = clusters.clusters[i].points
+    for (let j = 0; j < pointsArray.length; j++) {
+      if (pointsArray[j].includes(screenWidth) && pointsArray[j].includes(topBitrate)) {
+        location = i
+        break
+      }
+    }
+
+    if (location != -1) {
+      break
+    }
+  }
+
+  for (let i = resolutionWidth.length - 1; i >= 0; i--) {
+    if (resolutionWidth[i] <= clusters.centroids[location][0] &&
+      availableBitrates[i] <= clusters.centroids[location][1] * 1000) {
+      return availableBitrates[i]
+    }
+  }
+
+  return availableBitrates[0]
+}
+
+const getK = (data) => {
+  const screen_array = []
+
+  for (let i = 0; i < data.length; i++) {
+    if (screen_array.includes(data[i][0])) {
+    } else {
+      screen_array.push(data[i][0])
+    }
+  }
+
+  return screen_array.length
 }
